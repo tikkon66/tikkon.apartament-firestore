29d11ba44bddac3e1b85c673ea219d2fab0aa5df
Sabitov Tirek <153196289+tikkon66@users.noreply.github.com>
2026-03-30 05:59:37 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/ 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/tikkon.apartament-firestore/'
+  // Указываем название вашего репозитория между слэшами
+  base: '/tikkon.apartament-firestore/', 
 })
