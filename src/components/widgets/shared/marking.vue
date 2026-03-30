e58c8d0b311788fd74cbcaf85db4bdb37699c617
tikkon66 <sabitovtirek1609@gmail.com>
2026-03-30 06:01:41 +0500

    ff

---
+++ src/components/widgets/shared/marking.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+<template>
+        <Header />
+
+        <Footer/>
+
+</template>
+<style scoped>
+</style>
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/ 
+
 export default defineConfig({
   plugins: [vue()],
-  // Указываем название вашего репозитория между слэшами
-  base: '/tikkon.apartament-firestore/', 
+  base: '/tikkon.apartament-firestore/'
 })
