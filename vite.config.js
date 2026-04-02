43528b72bf437114b1263d3422dbde7e02519e79
Sabitov Tirek <153196289+tikkon66@users.noreply.github.com>
2026-04-02 14:16:24 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import path from 'path' // 1. Импортируем path
 
-// https://vite.dev/config/ 
-
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/tikkon.apartament-firestore/'
+  base: '/tikkon.apartament-firestore/', // Оставляем ваш путь для GitHub Pages
+  resolve: {
+    alias: {
+      // 2. Настраиваем алиас '@' для папки 'src'
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
 })
